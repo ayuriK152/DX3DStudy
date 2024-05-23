84b141a1d3f4cc26da16d2a7743e6e9613ba6719
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-05-23 22:52:02 +0900

    기존 공부 커밋

---
+++ DirectStudy/DirectStudy/Game.cpp
@@ -0,0 +1,68 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init(HWND hWnd)
+{
+	_hWnd = hWnd;
+	_width = GWinSizeX;
+	_height = GWinSizeY;
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+
+}
+
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width;
+		desc.BufferDesc.Height = _height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = _hWnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr,
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
+}
---
+++ DirectStudy/DirectStudy/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Game
+{
+public:
+	Game();
+	~Game();
+
+public:
+	void Init(HWND hWnd);
+	void Update();
+	void Render();
+
+private:
+	void CreateDeviceAndSwapChain();
+
+private:
+	HWND _hWnd;
+	uint32 _width = 0;
+	uint32 _height = 0;
+
+private:
+	// DX
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+};
+

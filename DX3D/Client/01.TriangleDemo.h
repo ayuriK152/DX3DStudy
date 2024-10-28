e2616a2d5e786022c8334cfa9b3865f510f4486b
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-10-28 18:17:31 +0900

    DX3D ConstBuffer까지

--- DX3D/Client/01.TriangleDemo.h
+++ DX3D/Client/01.TriangleDemo.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "IExecute.h"
 
 class TriangleDemo : public IExecute
 {
--- DX3D/Client/Main.cpp
+++ DX3D/Client/Main.cpp
@@ -2,6 +2,8 @@
 #include "Main.h"
 #include "Engine/Game.h"
 #include "01.TriangleDemo.h"
+#include "02.QuadDemo.h"
+#include "03.ConstBufferDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -13,7 +15,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 1.0f);
-	desc.app = make_shared<TriangleDemo>();
+	desc.app = make_shared<ConstBufferDemo>();
 
 	GAME->Run(desc);
 
